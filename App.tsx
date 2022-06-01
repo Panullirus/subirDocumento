a60ab8ddc9bdda89b4114cbb7329a7bb69d41df8
Yahir Gonzalez Torres <tyahir27@gmail.com>
2022-06-01 10:30:05 -0500

    Subir documentos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View } from 'react-native';
+import Amplify, { Storage } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+import * as DocumentPicker from 'expo-document-picker';
 
 export default function App() {
+
+   type DocumentFile = "pdf" | "jpeg" | "png";
+   type pickFileFn = () => Promise<string>;
+   type uploadFn = (fileType: DocumentFile, uri: string) => Promise<string>;
+
+   interface SlisstoDocuments {
+     pickDocument: pickFileFn
+     pickImage: pickFileFn
+     uploadFile: uploadFn
+   }
+
+  //  const uploadFile: uploadFn = async (fileType, uri) => {
+  //    const key = fileType;
+  //  }
+
+  //  export const SlisstoDocuments: SlisstoDocuments = {
+  //    uploadFile
+  //  }
+
+  async function selectDocument() {
+    var archivo:any = await DocumentPicker.getDocumentAsync({})
+    try {
+      const response = await fetch(archivo.uri);
+      const blob = await response.blob();
+      await Storage.put("pdf", blob)
+    }catch(err){
+      console.log("Error uploading file: ", err)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+      title="subir documento"
+      onPress={selectDocument}
+      />
     </View>
   );
 }
